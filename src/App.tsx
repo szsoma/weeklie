7098ee9f9adcf6ae797303c1a432d8f768ba7141
Soma Szoboszlai <somaszoboszlai@gmail.com>
2026-06-12 23:00:13 +0200

    feat: BacklogPanel persistent bottom section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import WeekHeader from './components/WeekHeader'
 import WeekGrid from './components/WeekGrid'
+import BacklogPanel from './components/BacklogPanel'
 
 export default function App() {
   return (
     <div className="h-screen flex flex-col">
       <WeekHeader />
       <div className="flex-1 flex flex-col min-h-0">
-        <WeekGrid />
+        <div className="flex-1 min-h-0">
+          <WeekGrid />
+        </div>
+        <BacklogPanel />
       </div>
     </div>
   )
---
+++ src/components/BacklogPanel.tsx
@@ -0,0 +1,25 @@
+import { useStore } from '../store'
+import TaskRow from './TaskRow'
+import NewTaskLine from './NewTaskLine'
+
+export default function BacklogPanel() {
+  const tasks = useStore(s =>
+    s.tasks
+      .filter(t => t.date === null)
+      .sort((a, b) => a.order - b.order)
+  )
+
+  return (
+    <div className="border-t border-black/10 flex flex-col min-h-[35vh]">
+      <div className="px-4 py-2">
+        <h2 className="text-sm font-mono text-gray-500">Backlog</h2>
+      </div>
+      <div className="flex-1 overflow-y-auto px-4 pb-2">
+        {tasks.map(task => (
+          <TaskRow key={task.id} task={task} />
+        ))}
+        <NewTaskLine date={null} />
+      </div>
+    </div>
+  )
+}
